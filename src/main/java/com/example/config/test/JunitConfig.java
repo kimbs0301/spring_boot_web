36afc6073dfbdb2876d62492a82c73dfb0e8d264
byeongsu kim <byeongsukim@byeongsus-Mac-mini.local>
2016-05-28 23:28:29 +0900

    ok

--- src/main/java/com/example/config/test/JunitConfig.java
+++ src/main/java/com/example/config/test/JunitConfig.java
@@ -13,8 +13,7 @@ import com.example.config.embedded.EmbeddedTomcatConfig;
 import com.example.main.Application;
 
 @Configuration
-@ComponentScan(basePackages = { "com.example.boot" }, excludeFilters = @Filter(value = { Application.class,
-		EmbeddedTomcatConfig.class, WebConfig.class}, type = FilterType.ASSIGNABLE_TYPE))
+@ComponentScan(basePackages = { "com.example.boot" }, excludeFilters = @Filter(value = { WebConfig.class}, type = FilterType.ASSIGNABLE_TYPE))
 @PropertySource(value = { "classpath:common.properties", "classpath:database.properties" })
 public class JunitConfig {
 	private static final Logger LOGGER = LoggerFactory.getLogger(JunitConfig.class);
